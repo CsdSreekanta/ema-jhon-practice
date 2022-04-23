bc0e04bb00a83c8cab7c722221e46d6fc21d52c1
unknown <sreekanta.csd@gmail.com>
2022-04-23 11:54:04 +0600

    product design updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -9,12 +9,17 @@ const Product = (props) => {
        
           <div className="product">
             <img src={img} alt="" />
-            <div>
-                <p>{name}</p>
-                <p>Price:${price}</p>
-                <p><small>Manufacturer:{seller}</small></p>
-                <p><small>Rating:{ratings}</small></p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price:${price}</p>
+                <p className='product-manufacturer'><small>Manufacturer{seller}</small></p>
+                <p className='product-rating'><small>Rating:{ratings}</small></p>
+                <button className='button-cart'>
+                    <p className='cart-text'> Add To Cart</p>=
+                </button>
             </div>
+           
+            
           </div>
         
     );
